714b9162cd5265b144d02d2b32f295f094b519b8
Ashish Kamal Bhati <ashishbhati26@gmail.com>
2024-03-15 18:01:48 +0530

    second commit

--- src/Card.js
+++ src/Card.js
@@ -9,7 +9,7 @@ export default function Card(props) {
         <img className=" w-full" src={props.imgsrc} alt="" />
         <div>
             {/* <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.title}</h5> */}
-            <p className="mb-3 font-normal text-white dark:text-gray-40 pt-2">{props.sname}</p>
+            <p className="mb-3 font-normal text-white dark:text-gray-40 pt-2 text">{props.sname}</p>
         </div>
         </a>
     </div>
